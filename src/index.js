f898b69dd1dbe95884bc9e73fcdaeb42101dc2d1
Akhtaruzzaman SM <54482784+aktarwd@users.noreply.github.com>
2023-04-01 01:04:21 +0600

    Update index.js
    Check

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './ifile.html';
-
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
